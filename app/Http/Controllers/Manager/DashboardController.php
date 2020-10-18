2f8a7d0485e02fd819d079e88e798f9aee2d9509
Son Dang <son.dang.kms@editshare.com>
2020-10-18 23:50:04 +0700

    code

--- app/Http/Controllers/Manager/DashboardController.php
+++ app/Http/Controllers/Manager/DashboardController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Bill;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('manager.dashboard');
+        $data['new_order'] = Bill::where('confirm', false)->count();
+        $data['confirmed'] = Bill::where('confirm', true)->whereNull('ship_date')->whereNull('receive_date')->count();
+        $data['delivery'] = Bill::whereNotNull('ship_date')->whereNull('receive_date')->count();
+        $data['completed'] = Bill::whereNotNull('receive_date')->count();
+        return view('manager.dashboard', $data);
     }
 }
--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -1,5 +1,129 @@
 @extends('manager.layouts.master')
 
+@section('title','Thống kê')
+
+@section('breadcrumbs')
+    @include('manager.layouts.components.breadcrumbs',[
+            'title_page' => 'Thống kê',
+            'sub_page' => [
+                ['url' => '', 'name' => 'Thống kê'],
+            ]
+    ])
+@endsection
+
 @section('main')
-    
+    <!--Begin::Dashboard 1-->
+
+<!--Begin::Row-->
+<div class="row">
+    <div class="col-lg-8 col-xl-8 order-lg-1 order-xl-1">
+
+        <!--begin:: Widgets/Activity-->
+        <div
+            class="kt-portlet kt-portlet--fit kt-portlet--head-lg kt-portlet--head-overlay kt-portlet--skin-solid kt-portlet--height-fluid">
+            <div class="kt-portlet__head kt-portlet__head--noborder kt-portlet__space-x">
+                <div class="kt-portlet__head-label">
+                    <h3 class="kt-portlet__head-title">
+                        Thống kê đơn hàng
+                    </h3>
+                </div>
+            </div>
+            <div class="kt-portlet__body kt-portlet__body--fit">
+                <div class="kt-widget17">
+                    <div class="kt-widget17__visual kt-widget17__visual--chart kt-portlet-fit--top kt-portlet-fit--sides"
+                        style="background-color: #fd397a">
+                        <div class="kt-widget17__chart" style="height:320px;">
+                            <canvas id="kt_chart_activities"></canvas>
+                        </div>
+                    </div>
+                    <div class="kt-widget17__stats">
+                        <div class="kt-widget17__items">
+                            <div class="kt-widget17__item">
+                                <span class="kt-widget17__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"/>
+                                            <path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3"/>
+                                            <path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000"/>
+                                            <rect fill="#000000" opacity="0.3" x="10" y="9" width="7" height="2" rx="1"/>
+                                            <rect fill="#000000" opacity="0.3" x="7" y="9" width="2" height="2" rx="1"/>
+                                            <rect fill="#000000" opacity="0.3" x="7" y="13" width="2" height="2" rx="1"/>
+                                            <rect fill="#000000" opacity="0.3" x="10" y="13" width="7" height="2" rx="1"/>
+                                            <rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
+                                            <rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
+                                        </g>
+                                    </svg></span>
+                                <span class="kt-widget17__subtitle">
+                                    Đơn hàng chờ xác nhận
+                                </span>
+                                <span class="kt-widget17__desc">
+                                    {{ $new_order }} đơn (<a href="{{route('manager.bills.tab', 'new')}}">Chi tiết</a>)
+                                </span>
+                            </div>
+                            <div class="kt-widget17__item">
+                                <span class="kt-widget17__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"/>
+                                            <path d="M4.875,20.75 C4.63541667,20.75 4.39583333,20.6541667 4.20416667,20.4625 L2.2875,18.5458333 C1.90416667,18.1625 1.90416667,17.5875 2.2875,17.2041667 C2.67083333,16.8208333 3.29375,16.8208333 3.62916667,17.2041667 L4.875,18.45 L8.0375,15.2875 C8.42083333,14.9041667 8.99583333,14.9041667 9.37916667,15.2875 C9.7625,15.6708333 9.7625,16.2458333 9.37916667,16.6291667 L5.54583333,20.4625 C5.35416667,20.6541667 5.11458333,20.75 4.875,20.75 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+                                            <path d="M2,11.8650466 L2,6 C2,4.34314575 3.34314575,3 5,3 L19,3 C20.6568542,3 22,4.34314575 22,6 L22,15 C22,15.0032706 21.9999948,15.0065399 21.9999843,15.009808 L22.0249378,15 L22.0249378,19.5857864 C22.0249378,20.1380712 21.5772226,20.5857864 21.0249378,20.5857864 C20.7597213,20.5857864 20.5053674,20.4804296 20.317831,20.2928932 L18.0249378,18 L12.9835977,18 C12.7263047,14.0909841 9.47412135,11 5.5,11 C4.23590829,11 3.04485894,11.3127315 2,11.8650466 Z M6,7 C5.44771525,7 5,7.44771525 5,8 C5,8.55228475 5.44771525,9 6,9 L15,9 C15.5522847,9 16,8.55228475 16,8 C16,7.44771525 15.5522847,7 15,7 L6,7 Z" fill="#000000"/>
+                                        </g>
+                                    </svg></span>
+                                <span class="kt-widget17__subtitle">
+                                    Đơn hàng đã xác nhận
+                                </span>
+                                <span class="kt-widget17__desc">
+                                    {{ $confirmed }} đơn (<a href="{{route('manager.bills.tab', 'confirmed')}}">Chi tiết</a>)
+                                </span>
+                            </div>
+                        </div>
+                        <div class="kt-widget17__items">
+                            <div class="kt-widget17__item">
+                                <span class="kt-widget17__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"/>
+                                            <path d="M4.88230018,17.2353996 L13.2844582,0.431083506 C13.4820496,0.0359007077 13.9625881,-0.12427877 14.3577709,0.0733126292 C14.5125928,0.15072359 14.6381308,0.276261584 14.7155418,0.431083506 L23.1176998,17.2353996 C23.3152912,17.6305824 23.1551117,18.1111209 22.7599289,18.3087123 C22.5664522,18.4054506 22.3420471,18.4197165 22.1378777,18.3482572 L14,15.5 L5.86212227,18.3482572 C5.44509941,18.4942152 4.98871325,18.2744737 4.84275525,17.8574509 C4.77129597,17.6532815 4.78556182,17.4288764 4.88230018,17.2353996 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.000087, 9.191034) rotate(-315.000000) translate(-14.000087, -9.191034) "/>
+                                        </g>
+                                    </svg></span>
+                                <span class="kt-widget17__subtitle">
+                                    Đơn hàng đang giao
+                                </span>
+                                <span class="kt-widget17__desc">
+                                    {{ $delivery }} (<a href="{{route('manager.bills.tab', 'delivery')}}">Chi tiết</a>)
+                                </span>
+                            </div>
+                            <div class="kt-widget17__item">
+                                <span class="kt-widget17__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24"/>
+                                            <circle fill="#000000" opacity="0.3" cx="12" cy="12" r="10"/>
+                                            <path d="M16.7689447,7.81768175 C17.1457787,7.41393107 17.7785676,7.39211077 18.1823183,7.76894473 C18.5860689,8.1457787 18.6078892,8.77856757 18.2310553,9.18231825 L11.2310553,16.6823183 C10.8654446,17.0740439 10.2560456,17.107974 9.84920863,16.7592566 L6.34920863,13.7592566 C5.92988278,13.3998345 5.88132125,12.7685345 6.2407434,12.3492086 C6.60016555,11.9298828 7.23146553,11.8813212 7.65079137,12.2407434 L10.4229928,14.616916 L16.7689447,7.81768175 Z" fill="#000000" fill-rule="nonzero"/>
+                                        </g>
+                                    </svg></span>
+                                <span class="kt-widget17__subtitle">
+                                    Đơn hàng đã hoàn thành
+                                </span>
+                                <span class="kt-widget17__desc">
+                                    {{ $completed }} đơn (<a href="{{route('manager.bills.tab', 'received')}}">Chi tiết</a>)
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--end:: Widgets/Activity-->
+    </div>
+    <div class="col-lg-4 col-xl-4 order-lg-1 order-xl-1">
+
+
+    </div>
+</div>
+
+<!--End::Row-->
+
+<!--End::Dashboard 1-->
 @endsection
