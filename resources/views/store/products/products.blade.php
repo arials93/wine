26a6653b118e81a8365e78d8f38f001274527277
Son Dang <son.dang.kms@editshare.com>
2020-10-18 23:40:06 +0700

    code

--- resources/views/store/products/products.blade.php
+++ resources/views/store/products/products.blade.php
@@ -5,7 +5,7 @@
 @push('styles')
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
 	<style>
-        .selectpicker {
+        .selectpicker-manual {
             color: #999;
             cursor: pointer;
             background-color: #e2e6ea;
@@ -63,28 +63,28 @@
 						<form class="categories" method="GET">
                             <h3>Tìm kiếm sản phẩm</h3>
                             @if (!request()->route('sub_category_id'))
-                                <select name="sub_category" class="selectpicker mb-4">
+                                <select name="sub_category" class="selectpicker-manual mb-4">
                                     <option value="0">Chọn loại sản phẩm</option>
                                     @foreach ($sub_categories as $item)
                                     <option {{ request()->sub_category == $item->id ? 'selected' : '' }} value="{{$item->id}}">{{$item->name}}</option>
                                     @endforeach
                                 </select>
                             @endif
-                            <select name="size" class="selectpicker mb-4">
+                            <select name="size" class="selectpicker-manual mb-4">
                                 <option value="0">Chọn dung tích</option>
                                 @foreach ($sizes as $item)
                                 <option {{ request()->size == $item->id ? 'selected' : '' }} value="{{$item->id}}">{{$item->size}}</option>
                                 @endforeach
                             </select>
 
-                            <select name="brand" class="selectpicker mb-4">
+                            <select name="brand" class="selectpicker-manual mb-4">
                                 <option value="0">Chọn nhãn hiệu</option>
                                 @foreach ($brands as $item)
                                 <option {{ request()->brand == $item->id ? 'selected' : '' }} value="{{$item->id}}">{{$item->name}}</option>
                                 @endforeach
                             </select>
 
-                            <input name="name" value="{{ request()->name ?? '' }}" class="selectpicker mb-4" placeholder="Nhập tên sản phẩm"/>
+                            <input name="name" value="{{ request()->name ?? '' }}" class="selectpicker-manual mb-4" placeholder="Nhập tên sản phẩm"/>
 
                             <button class="btn btn-primary w-100" type="submit">Tìm kiếm</button>
 						</form>
